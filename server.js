f87a1e53aadf2b7212fb1957219e08cbccd7aec2
Skoobieton <91810317+Tanyaskubon@users.noreply.github.com>
2022-01-29 12:04:26 -0500

    add rows

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+// conect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MYSQL username,
+        user: 'root',
+        // Your MYSQL PASSWOORD
+        password: '1$Ilikeithot',
+        database: 'election'
+    },
+    console.log (' Connected to the election databade.')
+);
 
 
 
 
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 
 
